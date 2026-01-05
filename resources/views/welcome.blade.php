186f018adf34d555e699182fbf42c03114dc7a54
Labib Awwam <l200220147@student.ums.ac.id>
2026-01-05 09:54:44 +0700

    fix homepage cdn

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,9 @@
     <link rel="icon" href="{{ asset('gambar/setyo1.jpg') }}" type="image/jpeg" sizes="32x32">
     <link rel="apple-touch-icon" href="{{ asset('gambar/setyo1.jpg') }}">
 
-    @vite('resources/css/app.css')
+    <!-- @vite(entrypoints: 'resources/css/app.css') -->
+         <script src="https://cdn.tailwindcss.com"></script>
+
 
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;800&family=Playfair+Display:ital,wght@0,700;1,700&display=swap" rel="stylesheet">
 
